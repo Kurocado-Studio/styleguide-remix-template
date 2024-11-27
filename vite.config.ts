60b8f18ce4b265b1f348f310627c59f4c70f470b
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2024-11-27 12:16:49 -0600

    chore(config): adds copyright

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/**
+ * Made with ❤️ and adobo by Kurocado Studio
+ * Copyright (c) 2024. All Rights Reserved.
+ *
+ * Learn more about Kurocado Studio: {@link https://www.kurocado.studio}
+ *
+ * Explore our open-source projects: {@link https://github.com/kurocado-studio}
+ */
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
