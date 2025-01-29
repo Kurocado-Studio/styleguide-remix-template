c3bf53ac3cf2fccf10af5869666e49cbde7c56fd
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-01-29 17:15:00 -0600

    chore(ui): update homepage STY-9

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,39 +22,34 @@ export const meta: MetaFunction = () => {
 
 export default function Index(): React.ReactNode {
   return (
-    <div
-      style={{
-        fontFamily: 'system-ui, sans-serif',
-        lineHeight: '1.8',
-      }}
-      className='bg-blue-500 text-white'
-    >
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/blog'
-            rel='noreferrer'
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target='_blank'
-            href='https://remix.run/tutorials/jokes'
-            rel='noreferrer'
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <main>
+      <div className='bg-white'>
+        <div className='px-6 py-24 sm:px-6 sm:py-32 lg:px-8'>
+          <div className='mx-auto max-w-2xl text-center'>
+            <h1 className='text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl'>
+              Kurocado Studio Remix starter template
+            </h1>
+            <p className='mx-auto mt-6 max-w-xl text-pretty text-lg/8 text-gray-600'>
+              A structured approach to writing clean, scalable, and maintainable
+              TypeScript code for modern web applications.
+            </p>
+            <div className='mt-10 flex items-center justify-center gap-x-6'>
+              <a
+                href='https://kurocado.youtrack.cloud/articles/STY-A-3/Overview'
+                className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              >
+                Learn more
+              </a>
+              <a
+                href='https://github.com/Kurocado-Studio/styleguide-remix-template'
+                className='text-sm/6 font-semibold text-gray-900'
+              >
+                View on GitHub<span aria-hidden='true'>→</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
