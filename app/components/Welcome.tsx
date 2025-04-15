9b3d82353eff10bcf13b34206e081cd959912a4d
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2025-04-14 22:32:26 -0500

    chore(config): updates url linking to case study

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -13,7 +13,7 @@ export function Welcome(): React.ReactNode {
         </p>
         <div className='mt-10 flex items-center justify-center gap-x-6'>
           <a
-            href='https://kurocado.youtrack.cloud/articles/STY-A-3/Overview'
+            href='https://kurocado-studio.github.io/platform/case-study.html'
             className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
           >
             Learn more
